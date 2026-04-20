642ef50ec488ef5688fefeaced1d3378c0a2c502
marymckee-dev <mary@coordinationhealth.com>
2026-04-20 12:15:28 -0400

    feat(oauth): add calendar.readonly scope to enable calendar listing

--- src/lib/server/google-oauth.ts
+++ src/lib/server/google-oauth.ts
@@ -4,6 +4,7 @@ export const SCOPES = [
   'https://www.googleapis.com/auth/gmail.readonly',
   'https://www.googleapis.com/auth/gmail.modify',
   'https://www.googleapis.com/auth/gmail.send',
+  'https://www.googleapis.com/auth/calendar.readonly',
   'https://www.googleapis.com/auth/calendar.events',
   'https://www.googleapis.com/auth/tasks',
   'https://www.googleapis.com/auth/drive.file',
